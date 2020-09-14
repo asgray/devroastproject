362545c468ee5792ec3746c51cbadf3c93c5e895
asgray <antgray89@gmail.com>
2020-09-13 22:21:44 -0700

    added nginx container, test.css is available

--- frontend/react-frontend/src/index.js
+++ frontend/react-frontend/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// import './index.css';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
+    <link
+      rel="stylesheet"
+      href="https://devroastproject_nginx:8080/usr/share/nginx/staticfiles/test.css"
+      // href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
